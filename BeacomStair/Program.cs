7db967589391b63407716a804cfe759d2d4102c9
Markg0192 <markg0192@hotmail.com>
2026-09-19 11:41:32 +0100

    Add Tekla model connection and stair pick workflow

---
+++ BeacomStair/Program.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Windows.Forms;
+using Tekla.Structures.Geometry3d;
+using Tekla.Structures.Model;
+using Tekla.Structures.Model.UI;
+
+namespace BeacomStair
+{
+    internal static class Program
+    {
+        [STAThread]
+        private static void Main()
+        {
+            try
+            {
+                Model model = new Model();
+
+                if (!model.GetConnectionStatus())
+                {
+                    MessageBox.Show(
+                        "Open Tekla Structures 2023 and a model first, then run BeacomStair again.",
+                        "BeacomStair",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Warning);
+                    return;
+                }
+
+                Picker picker = new Picker();
+
+                Point edgeStart = picker.PickPoint(
+                    "BeacomStair: pick the FIRST point on the upper-floor / platform back edge.");
+
+                Point edgeEnd = picker.PickPoint(
+                    "BeacomStair: pick the SECOND point on the upper-floor / platform back edge.");
+
+                Point towardFoot = picker.PickPoint(
+                    "BeacomStair: pick any point in the direction you want the stair FOOT to run.");
+
+                StairBuilder builder = new StairBuilder(model);
+                StairBuildResult result = builder.Build(edgeStart, edgeEnd, towardFoot);
+
+                model.CommitChanges();
+
+                MessageBox.Show(
+                    "Beacom stair inserted.\r\n\r\n" +
+                    "15 rises @ " + result.Rise.ToString("0.00") + " mm\r\n" +
+                    "14 treads @ " + StairSettings.Going.ToString("0") + " mm\r\n" +
+                    "Flight run: " + StairSettings.FlightRun.ToString("0") + " mm\r\n" +
+                    "Top platform: " + StairSettings.PlatformLength.ToString("0") + " mm\r\n" +
+                    "Overall: " + StairSettings.OverallLength.ToString("0") + " mm\r\n" +
+                    "Objects inserted: " + result.InsertedObjectCount,
+                    "BeacomStair",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(
+                    "BeacomStair stopped.\r\n\r\n" + ex.Message,
+                    "BeacomStair",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+            }
+        }
+    }
+}
